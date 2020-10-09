9b3bb0b9e7dcdf622cb7b9f6b5421d843106625b
Olaemmy <eolatunde03@gmail.com>
2020-10-09 09:12:11 -0700

    keyboard_layout

---
+++ keybord.js
@@ -0,0 +1,11 @@
+// let button = document.getElementById("spacebar")
+// container.onclick = function(spacebar){
+//   alert('You just clicked SPACEBAR');
+// };
+
+document.getElementById("enterkey").addEventListener("click", function(){
+  alert('enter clicked')
+})
+document.getElementById("spacebar").addEventListener("click", function(spacebar){
+  alert('spacebar clicked')
+})
